e6db9850de60a65b5235efd2e7047b84141a52f0
natj <nattila.joonas@gmail.com>
2017-05-12 09:48:11 +0300

    handle limits in lineprof

--- lineprofile.py
+++ lineprofile.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-
+import matplotlib.pyplot as plt
 
 
 
@@ -12,6 +12,11 @@ def lineprofile(fluxc, redsc):
     yarr = np.array([])
 
     Ny_dense, Nx_dense = np.shape(fluxc)
+
+    #print "Mean redshift={}".format(np.mean(redsc))
+    #print "Median redshift={}".format(np.median(redsc))
+    #print "std redshift={}".format(np.std(redsc))
+
 
     energ = 1.0
     for jj in range(Ny_dense):
@@ -28,10 +33,17 @@ def lineprofile(fluxc, redsc):
     xarrs = xarr[xind]
     yarrs = yarr[xind]
 
-    NN = len(xarrs)
+    #plt.plot(xarrs, yarrs)
+    #plt.plot(xarrs)
+    #plt.show()
 
-    emin = np.min(xarrs)*0.99
-    emax = np.max(xarrs)*1.01
+    #Discard 0.5% of tails (=nonsense values)
+    NN = len(xarrs)
+    emin = xarrs[np.int(0.005*NN)]
+    emax = xarrs[np.int(0.995*NN)]
+
+    #emin = np.min(xarrs)*0.99
+    #emax = np.max(xarrs)*1.01
 
     Nr = 100
     es = np.linspace(emin, emax, Nr)
@@ -45,6 +57,14 @@ def lineprofile(fluxc, redsc):
             elif xarrs[jj] >= es[ii]:
                 xst = jj
                 break
+
+    #close the profile
+    es = np.append(es, es[-1]+0.001)
+    yy2 = np.append(yy2, 0.0)
+
+    es = np.insert(es, 0, es[0]-0.001)
+    yy2 = np.insert(yy2, 0, 0.0)
+
 
     #normalize
     des = np.diff(es)[1]
